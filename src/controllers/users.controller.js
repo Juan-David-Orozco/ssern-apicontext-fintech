cd84709eb096c5e005559e8bc244642dc5da1e58
Juan-David-Orozco <ing.ele2018@gmail.com>
2023-03-21 16:21:06 -0500

    config CRUD - users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -13,6 +13,21 @@ export const getUsers = async (req, res) => {
   }
 }
 
+export const getUser = async (req, res) => {
+  try {
+    const pool = await connectDB()
+    const response = await pool.request()
+      .input('i_id', sql.Int, req.params.id)
+      .execute('getUser')    
+    const user = response.recordset[0]
+    console.log(user)
+    if(!user) return res.status(404).json({msg: "User not found"})
+    res.status(200).json({msg: "Get User", user})
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+}
+
 export const createUser = async (req, res) => {
   const { username, email, password } = req.body
   let { phone, city, address, type } = req.body
@@ -25,53 +40,12 @@ export const createUser = async (req, res) => {
   else if(type === "customer" || type === undefined) type = 2
   else {return res.status(400).json({msg: "UserType not exists"})}  
   // parametros opcionales (phone, city,address)
-  if(phone === undefined) phone = "NULL"
-  if(city === undefined) city = "NULL"
-  if(address === undefined) address = "NULL"
+  // if(phone === undefined) phone = "NULL"
+  // if(city === undefined) city = "NULL"
+  // if(address === undefined) address = "NULL"
   // mostrar consola - parametros requeridos y opcionales
-  console.log(phone, city, address)
   console.log(username, email, password, type)
-
-  // config campos DATETIME
-  const currentDate = new Date()
-  const currentNow = Date.now()
-  console.log(currentDate)
-  console.log(currentNow)
-
-  console.log(currentDate.toString())
-  console.log(currentDate.toDateString())
-  console.log(currentDate.toISOString())
-  console.log(JSON.stringify(currentDate.toJSON()))
-  console.log(currentDate.toLocaleString())
-  console.log(currentDate.toLocaleDateString())
-  console.log(currentDate.toLocaleTimeString())
-  
-  
-
-  //console.log(currentDate.getDate(), currentDate.getHours())
-
-  // const momentDate = moment(new Date()).format()
-  // const momentNow = moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a')
-  // const momentFormat = moment(Date.now()).format('L')
-  // console.log(date)
-  // console.log(dateNow)
-  // console.log(momentDate)
-  
-  const date = `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`
-  const hour = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}.${currentDate.getMilliseconds()}`
-  
-  console.log(date)
-  console.log(hour)
-  const date_hour = `${date} ${hour}`
-  console.log(date_hour)
-
-  //res.status(201).json({msg: "New user created"})
-
-  // const date = currentDate.toLocaleDateString()
-  // const hour = currentDate.toLocaleTimeString()
-  // const dateYhour = `${date} ${hour}`
-  // console.log(dateYhour)
-
+  console.log(phone, city, address)
   try {
     const pool = await connectDB()
     await pool.request()
@@ -83,11 +57,67 @@ export const createUser = async (req, res) => {
       .input('i_phone', sql.VarChar(100), phone)
       .input('i_city', sql.VarChar(100), city)
       .input('i_address', sql.VarChar(100), address)
-      //.input('i_createdAt', sql.DateTime, date_hour)
-      //.input('i_updatedAt', sql.DateTime, date_hour)
       .execute('createNewUser')
     res.status(201).json({msg: "New user created"})
   } catch (error) {
     res.status(500).json({error: error.message})
   }
+}
+
+export const deleteUser = async (req, res) => {
+  try {
+    const pool = await connectDB()
+    // Found user if exists
+    const response = await pool.request()
+      .input('i_id', sql.Int, req.params.id)
+      .execute('getUser')
+    const userFound = response.recordset[0]
+    // If no exists return status 404
+    if(!userFound) return res.status(404).json({msg: "User not found"})
+    // If exists delete user found
+    await pool.request()
+      .input('i_id', sql.Int, req.params.id)
+      .execute('deleteUser')
+    res.status(204).json()
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+}
+
+export const updateUser = async (req, res) => {
+  let { username, email, phone, city, address } = req.body
+  console.log(username, email, phone, city, address)
+  try {
+    const pool = await connectDB()
+    // Found user if exists
+    const response = await pool.request()
+      .input('i_id', sql.Int, req.params.id)
+      .execute('getUser')
+    const userFound = response.recordset[0]
+    console.log(userFound)
+    // If no exists return status 404
+    if(!userFound) return res.status(404).json({msg: "User not found"})
+    if(username === undefined) username = userFound.username
+    if(email === undefined) email = userFound.email
+    if(phone === undefined) phone = userFound.phone
+    if(city === undefined) city = userFound.city
+    if(address === undefined) address = userFound.address
+    console.log(username, email, phone, city, address)
+    await pool.request()
+      .input('i_id', sql.Int, req.params.id)
+      .input('i_username', sql.VarChar(50), username)
+      .input('i_email', sql.VarChar(100), email)
+      .input('i_phone', sql.VarChar(100), phone)
+      .input('i_city', sql.VarChar(100), city)
+      .input('i_address', sql.VarChar(100), address)
+      .execute('updateUser')
+    res.status(201).json({msg: "Update user"})
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+}
+
+export const getTotalUsers = async (req, res) => {
+  console.log("Get Total User")
+  res.status(200).json({msg: "The total users are"})
 }
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { getUsers, createUser } from '../controllers/users.controller'
+import { getUsers, createUser, deleteUser, updateUser, getUser, getTotalUsers } from '../controllers/users.controller'
 
 const router = Router()
 
@@ -7,12 +7,12 @@ router.get('/users', getUsers)
 
 router.post('/users', createUser)
 
-//router.get('/users/count', getTotalUsers)
+router.get('/users/count', getTotalUsers)
 
-// router.get('/users/:id', getUser)
+router.get('/users/:id', getUser)
 
-// router.delete('/users/:id', deleteUser)
+router.delete('/users/:id', deleteUser)
 
-// router.put('/users/:id', updateUser)
+router.put('/users/:id', updateUser)
 
 export default router
